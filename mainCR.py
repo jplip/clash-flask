1b7c5d4b64696ca8d7d92a67fd1a10896fc90b77
jplip <jpliporada@gmail.com>
2023-10-19 13:04:39 -0700

    templete changes for cr api

---
+++ mainCR.py
@@ -0,0 +1,46 @@
+import threading
+
+# import "packages" from flask
+from flask import render_template  # import render_template from "public" flask libraries
+
+# import "packages" from "this" project
+from __init__ import app,db  # Definitions initialization
+from model.jokesCR import initStats
+
+# setup APIs
+from api.jokeCR import stats_api
+
+# setup App pages
+from projects.projects import app_projects # Blueprint directory import projects definition
+
+
+# Initialize the SQLAlchemy object to work with the Flask app instance
+db.init_app(app)
+
+# register URIs
+app.register_blueprint(stats_api)
+app.register_blueprint(app_projects) # register app pages
+
+@app.errorhandler(404)  # catch for URL not found
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+@app.route('/')  # connects default URL to index() function
+def index():
+    return render_template("index.html")
+
+@app.route('/table/')  # connects /stub/ URL to stub() function
+def table():
+    return render_template("table.html")
+
+@app.before_first_request
+def activate_job():  # activate these items
+    initStats()
+
+# this runs the application on the development server
+if __name__ == "__main__":
+    # change name for testing
+    from flask_cors import CORS
+    cors = CORS(app)
+    app.run(debug=True, host="0.0.0.0", port="8113")
---
+++ model/jokesCR.py
@@ -0,0 +1,144 @@
+import random
+
+card_data = []
+card_list = [
+    "Knight"
+    "Archers",
+    "Goblins",
+    "Giant",
+    "P.E.K.K.A",
+    "Minions",
+    "Balloon",
+    "Witch",
+    "Barbarians",
+    "Golem",
+    "Skeletons",
+    "Valkyrie",
+    "Skeleton Army",
+    "Bomber",
+    "Musketeer",
+    "Baby Dragon",
+    "Prince",
+    "Wizard",
+    "Mini P.E.K.K.A",
+    "Spear Goblins",
+    "Giant Skeleton",
+    "Hog Rider",
+    "Minion Horde",
+    "Ice Wizard",
+    "Royal Giant",
+    "Guards",
+    "Princess",
+    "Dark Prince",
+    "Three Musketeers",
+    "Lava Hound",
+    "Ice Spirit",
+    "Fire Spirit",
+    "Miner",
+    "Sparky",
+    "Bowler",
+    "Lunberjack",
+    "Battle Ram",
+    "Inferno Dragon",
+    "Ice Golem",
+    "Mega Minion",
+    "Dart Goblin",
+    "Goblin Gang",
+    "Electro Wizard",
+    "Elite Barbarians",
+    "Hunter",
+    "Executioner",
+    "Bandit",
+    "Royal Recruits",
+    "Night Witch",
+    "Bats",
+    "Royal Ghost",
+    "Ram Rider",
+    "Zappies",
+    "Rascals",
+]
+
+# Initialize jokes
+def initStats():
+    # setup jokes into a dictionary with id, joke, haha, boohoo
+    item_id = 0
+    for item in card_list:
+        card_data.append({"id": item_id, "joke": item, "haha": 0, "boohoo": 0})
+        item_id += 1
+    # prime some haha responses
+    for i in range(10):
+        id = getRandomJoke()['id']
+        addJokeHaHa(id)
+    # prime some haha responses
+    for i in range(5):
+        id = getRandomJoke()['id']
+        addJokeBooHoo(id)
+        
+# Return all jokes from jokes_data
+def getJokes():
+    return(card_data)
+
+# Joke getter
+def getJoke(id):
+    return(card_data[id])
+
+# Return random joke from jokes_data
+def getRandomJoke():
+    return(random.choice(card_data))
+
+# Liked joke
+def favoriteJoke():
+    best = 0
+    bestID = -1
+    for joke in getJokes():
+        if joke['haha'] > best:
+            best = joke['haha']
+            bestID = joke['id']
+    return card_data[bestID]
+    
+# Jeered joke
+def jeeredJoke():
+    worst = 0
+    worstID = -1
+    for joke in getJokes():
+        if joke['boohoo'] > worst:
+            worst = joke['boohoo']
+            worstID = joke['id']
+    return card_data[worstID]
+
+# Add to haha for requested id
+def addJokeHaHa(id):
+    card_data[id]['haha'] = card_data[id]['haha'] + 1
+    return card_data[id]['haha']
+
+# Add to boohoo for requested id
+def addJokeBooHoo(id):
+    card_data[id]['boohoo'] = card_data[id]['boohoo'] + 1
+    return card_data[id]['boohoo']
+
+# Pretty Print joke
+def printJoke(joke):
+    print(joke['id'], joke['joke'], "\n", "haha:", joke['haha'], "\n", "boohoo:", joke['boohoo'], "\n")
+
+# Number of jokes
+def countJokes():
+    return len(card_data)
+
+# Test Joke Model
+if __name__ == "__main__": 
+    initStats()  # initialize jokes
+    
+    # Most likes and most jeered
+    best = favoriteJoke()
+    print("Most liked", best['haha'])
+    printJoke(best)
+    worst = jeeredJoke()
+    print("Most jeered", worst['boohoo'])
+    printJoke(worst)
+    
+    # Random joke
+    print("Random joke")
+    printJoke(getRandomJoke())
+    
+    # Count of Jokes
+    print("Jokes Count: " + str(countJokes()))
