7ae00c7dc9b8993a69bba4c62235ef644188d3fb
jplip <jpliporada@gmail.com>
2023-10-27 15:57:45 -0700

    Clash royale actual api

---
+++ api/cards.py
@@ -0,0 +1,52 @@
+from contextlib import nullcontext
+from flask import Blueprint, jsonify  # jsonify creates an endpoint response object
+from flask_restful import Api, Resource # used for REST API building
+import time
+from flask import Blueprint, request, jsonify
+import json
+import requests
+
+
+cards_api = Blueprint('cards_api', __name__,
+                  url_prefix='/api/cards')
+
+# API docs https://flask-restful.readthedocs.io/en/latest/api.html
+api = Api(cards_api)
+
+def beautify_json_data(json_file_path):
+    try:
+        with open(json_file_path, 'r') as json_file:
+            data = json.load(json_file)
+
+        beautified_data = []
+        for item in data.get('items', []):
+            beautified_item = {
+                "name": item.get("name", ""),
+                "maxLevel": item.get("maxLevel", 0),
+            }
+
+            medium_icon_url = item.get("iconUrls", {}).get("medium", "")
+            if medium_icon_url:
+                beautified_item["medium"] = medium_icon_url
+
+            beautified_data.append(beautified_item)
+
+        return beautified_data  # Return the processed data as a list
+
+    except FileNotFoundError:
+        return {"error": "File not found"}
+    except json.JSONDecodeError:
+        return {"error": "Invalid JSON format in the file"}
+
+
+beautify_json_data("carddb.json")
+
+# getJokes()
+class _Read(Resource):
+    def get(self):
+        json_list = []
+        json_list.append(beautify_json_data('carddb.json'))
+        return jsonify(json_list)
+
+
+api.add_resource(_Read, '/')
