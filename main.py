ef9d72e699c71481ef77086abb66985d296c3518
beijanm <beijanmoniza@gmail.com>
2023-10-27 10:31:48 -0700

    Clash royale actual api

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from api.covid import covid_api # Blueprint import api definition
 from api.joke import joke_api # Blueprint import api definition
 from api.user import user_api # Blueprint import api definition
 from api.player import player_api
+from api.cards import cards_api
 
 
 # setup App pages
@@ -30,7 +31,7 @@ app.register_blueprint(covid_api) # register api routes
 app.register_blueprint(user_api) # register api routes
 app.register_blueprint(player_api)
 app.register_blueprint(app_projects) # register app pages
-
+app.register_blueprint(cards_api)
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
     # note that we set the 404 status explicitly
