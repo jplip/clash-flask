006ac20b08e300db43ad5dac5210612ed44572ee
beijanm <beijanmoniza@gmail.com>
2023-11-01 20:48:15 -0700

    more changes /rand not working

--- model/cards.py
+++ model/cards.py
@@ -1,48 +1,41 @@
+from flask import Flask, Blueprint, request, jsonify
+from flask_restful import Api, Resource
 import random
 import json
 
+app = Flask(__name__)
+
+cards_api = Blueprint('cards_api', __name__, url_prefix='/api/cards')
+api = Api(cards_api)
+
 card_data = []
 
-# Initialize cards
 def initCards():
     global card_data
     try:
         with open("carddb.json", "r") as file:
             card_data = json.load(file)
-        print("Successfully loaded JSON data:", card_data)
+            print("Successfully loaded JSON data:", card_data)
     except FileNotFoundError:
         card_data = []
         print("File not found.")
     except Exception as e:
         print("Error:", e)
-  # Initialize an empty list if the file is not found
 
-# Call initCards() to load the card data from carddb.json
-initCards()
+class RandomCard(Resource):
+    def get(self):
+        initCards()  # Ensure card_data is up to date
+        random_card = generateRandomCard()
+        return jsonify(random_card)
 
-def getCard():
-    return(card_data)
-
-# Define the getCard function
-def getCard(id):
-    for card in card_data:
-        if card["id"] == id:
-            return card
-        
-# Generate a random card
 def generateRandomCard():
-    print(card_data)  # Add this line to check the contents of card_data
-    return random.choice(card_data)
+    if card_data:
+        return random.choice(card_data)
+    else:
+        return {"error": "No card data available"}
 
-# Number of cards
-def countCards():
-    return len(card_data)
+api.add_resource(RandomCard, '/rand')
 
-# Test Card Model
 if __name__ == "__main__":
-
-    # Random card
-    print("Random card:")
-    random_card = generateRandomCard()
-    print(random_card)
-
+    app.register_blueprint(cards_api)
+    app.run(debug=True)
